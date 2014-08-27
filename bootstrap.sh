32b9a6587f7b252acaf2730cb4587718b1263197
matsu-chara <matsuy00@gmail.com>
2014-08-28 06:15:52 +0900

    add nginx mysql php

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-curl -L -O http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
-curl -L -O http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-curl -L -O http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-rpm -ivh epel-release-6-8.noarch.rpm rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm remi-release-6.rpm
-rm -f epel-release-6-8.noarch.rpm rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm remi-release-6.rpm
+rpm -ivh http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
 
 yum update
 yum groupinstall -y "Development Tools"
 
-yum install -y git
+# yum install -y httpd
+# /etc/init.d/httpd start
+
+yum install -y nginx
+
+yum install -y --enablerepo=epel mysql mysql-devel mysql-server mysql-utilities
+/etc/init.d/mysqld start
+yum install -y --enablerepo=remi,remi-php55 php php-opcache php-devel php-mbstring php-mcrypt php-mysqlnd php-pdo php-pecl-xdebug php-cli php-common php-pecl-apcu php-fpm
+
+
 git clone https://github.com/matsu-chara/dotfiles/ /home/vagrant/.dotfiles
 chown -R vagrant:vagrant /home/vagrant/.dotfiles
 
