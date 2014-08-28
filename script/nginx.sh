13d450ed13ca1009bf8c542c8e7a49b303e64a04
matsu-chara <matsuy00@gmail.com>
2014-08-29 02:32:32 +0900

    add resources

--- script/nginx.sh
+++ script/nginx.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
 yum install -y nginx
+
+cp -f /vagrant/resource/etc/nginx/cond.d/default.conf /etc/nginx/conf.d/default.conf
+
 service nginx start
 chkconfig nginx on
+
--- script/php55.sh
+++ script/php55.sh
@@ -2,5 +2,8 @@
 
 yum install -y --enablerepo=remi,remi-php55 php php-opcache php-devel php-mbstring php-mcrypt php-mysqlnd php-pdo php-pecl-xdebug php-cli php-common php-pecl-apcu php-fpm
 
+cp -f /vagrant/resource/etc/php.ini /etc/php.ini
+cp -f /vagrant/resource/etc/php-fpm.d/www.conf /etc/php-fpm.d/www.conf
+
 curl -sS https://getcomposer.org/installer | php
 mv -f composer.phar /usr/local/bin/composer
