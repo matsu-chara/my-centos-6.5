4e7ff627920a10993a7dd149f4092d9446533600
matsu-chara <matsuy00@gmail.com>
2014-08-30 16:25:27 +0900

    change resource to template

--- script/nginx.sh
+++ script/nginx.sh
@@ -2,7 +2,7 @@
 rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
 yum install -y nginx
 
-cp -f /vagrant/resource/etc/nginx/cond.d/default.conf /etc/nginx/conf.d/default.conf
+cp -f /vagrant/resource/etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf
 
 service nginx start
 chkconfig nginx on
